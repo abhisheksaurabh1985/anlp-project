27e2a0d11d027a9b33309fdab1a938fbb7bb9a34
abhisheksaurabh1985 <abhishek.saurabh@est.fib.upc.edu>
2016-04-27 22:57:55 +0200

    First commit

--- execute.py
+++ execute.py
@@ -8,3 +8,32 @@ dictAnnotatedData= getCSVInDictionary(csvFileName)
 # Word2Vec expects single sentences, each one of them as a list of words. Generate tokens from sentences. 
 tokenizedSentences= getTokens(dictAnnotatedData['Sentence'][0:])
 print len(tokenizedSentences)
+
+# Import the built-in logging module and configure it so that Word2Vec creates nice output messages
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+# Set values for various parameters
+num_features = 300    # Word vector dimensionality                      
+min_word_count = 2    # Minimum word count                        
+num_workers = 4       # Number of threads to run in parallel
+context = 2           # Context window size                                                                                    
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Initialize and train the model (this will take some time)
+from gensim.models import word2vec
+print "Training model..."
+model = word2vec.Word2Vec(tokenizedSentences, workers=num_workers, \
+            size=num_features, min_count = min_word_count, \
+            window = context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling 
+# init_sims will make the model much more memory-efficient.
+model.init_sims(replace=True)
+
+# It can be helpful to create a meaningful model name and 
+# save the model for later use. You can load it later using Word2Vec.load()
+model_name = "300features_2minwords_2context"
+model.save(model_name)
+
--- function_definitions.py
+++ function_definitions.py
@@ -5,6 +5,12 @@ import csv
 from nltk.tokenize import word_tokenize
 
 def getCSVInDictionary(csvFileName):
+    '''
+    Description: Read a multi column CSV file with first row as header. File content is stored in a dictionary which the function returns. 
+    Input: File path along with the file name.
+    Output: An ordered dictionary with file header as KEY. Dictionary values are a list which hold the column values corresponding to each header.
+    For e.g. {'header A': ['val_1_header_A', 'val_2_header_A'], 'header B': ['val_1_header_B', 'val_1_header_B']}   
+    '''
     reader= csv.DictReader(open(csvFileName))
     result= OrderedDict()
     for row in reader:
@@ -13,9 +19,14 @@ def getCSVInDictionary(csvFileName):
     return result
     
 def getTokens(listSentences):
+    '''
+    Description: Function to get tokens from sentences. Word2Vec expects single sentences, each one of them as a list of words.
+    Input: List of sentences.
+    Output: A nested list with the inner list containing a sentence in a tokenized form. All the tokens are in lowercase.
+    '''
     tokenizedSentences = []
     for eachSentence in listSentences:
-        tokenizedSentences.append(word_tokenize(eachSentence))
+        tokenizedSentences.append(word_tokenize(eachSentence.lower()))
     return tokenizedSentences
         
     
